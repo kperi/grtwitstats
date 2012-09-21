c0258374908c9fb060d3ef97d70d3b68e59a7189
Kostas Perifanos, aka kPeri <kostas.perifanos@gmail.com>
2012-09-21 23:00:35 +0100

    followers script

---
+++ getFollowers.py
@@ -0,0 +1,21 @@
+def getFollowers():
+	screen_name = sys.argv[1]
+	max_pages = 4 #number of pages (eg loops), change this to fit the account
+	next_cursor = -1
+
+	followers_ids = []
+
+	for i in range(0,max_pages):
+		url = 'https://api.twitter.com/1/followers/ids.json?screen_name=%s&cursor=%s' % (screen_name, next_cursor)
+		content = requests.get(url).content
+		data = json.loads(content)
+		next_cursor = data['next_cursor']
+		print "loop " , i, "cursor is", next_cursor
+		followers_ids.extend(data['ids'])
+
+        print "%s have %s followers!" % (screen_name, str(len(followers_ids)))
+
+        f = open( screen_name + '.txt', 'w+')
+	for item in followers_ids:
+        f.write("%s\n" % item)
+        f.close()
